3aae694fea29f055c73dd15273f37940ce1c7fb6
PabloCruz201120 <klabitos@usal.es>
2022-02-07 13:37:26 +0100

    [MOD se ordena la tabla por puntos]

--- proyecto_final/src/views/Clasificacion.vue
+++ proyecto_final/src/views/Clasificacion.vue
@@ -3,22 +3,22 @@
         <table class="table table-dark table-striped mt-3">
             <thead>
                 <tr>
-                <th scope="col">Posición</th>
-                <th scope="col"></th>
-                <th scope="col">Nombre</th>
-                <th scope="col">País</th>
-                <th scope="col">Puntos</th>
+                <th scope="col" class="col-2">Posición</th>
+                <th scope="col" class="col-1"></th>
+                <th scope="col" class="col-4">Nombre</th>
+                <th scope="col" class="col-1 offset-3">País</th>
+                <th scope="col" class="col-1">Puntos</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(equipo, index) in this.listaEquipos" :key="index">
+                <tr v-for="(equipo, index) in ordenarEquipos()" :key="index">
                     <th scope="row">{{index+1}}</th>
                     <td>
-                          <img :src="require('../assets/escudos/'+equipo.id+'.png')" alt="Girl in a jacket" width="50" height="50">  
+                        <img :src="require('../assets/escudos/'+equipo.id+'.png')" alt="Escudo" width="50" height="50">  
                     </td>
                     <td>{{equipo.name}}</td>
-                    <td>{{equipo.path}}</td>
-                    <td>21</td>
+                    <td>{{equipo.country}}</td>
+                    <td>{{equipo.points}}</td>
                 </tr>
             </tbody>
         </table>
@@ -32,15 +32,20 @@ export default {
     data(){
         return{
             listaEquipos:[],
-            image: require("../assets/escudos/2.png")
         }
     },
     methods: {
-        obtenerListaEquipos(){
-            axios.get("http://localhost:3000/clubs")
+        async obtenerListaEquipos(){
+            await axios.get("http://localhost:3000/clubs")
             .then(response => this.listaEquipos = response.data)
             .catch(response => alert("Error al recuperar datos"+ response.status));
         },
+        ordenarEquipos(){
+            this.listaEquipos.sort(function(a,b){
+                return b.points-a.points;
+            });
+            return this.listaEquipos;
+        }
     },
     created() {
         this.obtenerListaEquipos();
